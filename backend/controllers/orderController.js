2a0b48cb928f40f1c3fb3118d8598bc069c56075
codefather99 <emefokamsi@gmail.com>
2024-09-27 01:33:47 +0100

    Update pincher 2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import Order from '../models/orderModel.js';
-import Product from '../models/productModel.js';
+import Order from '../Models/orderModel.js';
+import Product from '../Models/productModel.js';
 import { calcPrices } from '../utils/calcPrices.js';
 import { verifyPayPalPayment, checkIfNewTransaction } from '../utils/paypal.js';
 
